85abf84d59daab4022c13d6aff5e366bb0d7d7f5
Timur Sultanov <tssultanov@outlook.com>
2025-02-13 01:54:02 +0400

    Icrease default window size

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() -> eframe::Result {
 
     let native_options = NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
+            .with_inner_size([1270.0, 740.0])
             .with_min_inner_size([300.0, 220.0]),
         vsync: true,
         ..Default::default()
