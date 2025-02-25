ad31a985cd531d0ffc74fa6029708321906f1bfe
Timur Sultanov <tssultanov@outlook.com>
2025-02-25 21:11:50 +0400

    Move Quad to a separate file

--- src/quad_mesh.rs
+++ src/quad_mesh.rs
@@ -3,3 +3,4 @@ pub mod quad_tree;
 pub mod point;
 pub mod polygon;
 pub mod mesh;
+pub mod quad;
--- src/quad_mesh/mesh.rs
+++ src/quad_mesh/mesh.rs
@@ -2,40 +2,7 @@ use crate::quad_mesh::{quad_tree::QuadTree, point::Point};
 use crate::quad_mesh::polygon::Polygon;
 
 use super::aabb::AABB;
-
-/// Represents a quadrilateral defined by its four vertices.
-pub struct Quad {
-    pub bottom_left: Point,
-    pub bottom_right: Point,
-    pub top_right: Point,
-    pub top_left: Point,
-}
-
-impl Quad {
-    /// Constructs a new `Quad` given the center and half dimensions.
-    pub fn new_rect(center: &Point, half_width: f32, half_height: f32) -> Self {
-        let left = center.x - half_width;
-        let right = center.x + half_width;
-        let bottom = center.y - half_height;
-        let top = center.y + half_height;
-        Quad {
-            bottom_left: Point { x: left, y: bottom },
-            bottom_right: Point { x: right, y: bottom },
-            top_right: Point { x: right, y: top },
-            top_left: Point { x: left, y: top },
-        }
-    }
-    
-    /// Returns an array of all vertices in counter-clockwise order starting from bottom-left.
-    pub fn vertices(&self) -> [Point; 4] {
-        [
-            self.bottom_left,
-            self.bottom_right,
-            self.top_right,
-            self.top_left,
-        ]
-    }
-}
+use super::quad::Quad;
 
 /// Mesh data structure based on quad tree mesh in structure-of-arrays format.
 pub struct Mesh {
